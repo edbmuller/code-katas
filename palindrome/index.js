af21d5bb8186f209714ea730dbaee0f082e2a20c
Eduardo Müller <edbmuller@gmail.com>
2019-05-20 23:10:01 -0300

    Implemented simple UI

--- palindrome/index.js
+++ palindrome/index.js
@@ -1,23 +1,29 @@
-
 //  PALINDROME KATA!
 //  """""""""""""""""
 //  Get a string and check if its palindrome, beeing an word or frase that may contain non-alphanumerics characteres.
 
+var form = document.querySelector('.form');
+var input = document.querySelector('.input');
+var output = document.querySelector('.output');
 
-  
-/* 05.20.19 - 40 min */
+form.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  palindrome(input.value);
+  input.value = '';
+});
 
-function palindrome(str) {
-
-  var strCleaned = str.replace(/[\Ws_]/g, '').toLowerCase();
-  var strTested = strCleaned.split('').reverse().join("");
-  if ( strCleaned === strTested ) { return console.log(`É palindromo: ${strTested}`); }
-  
-  return console.log(`Não é palindromo: ${strTested}`);
+const print = (templateStr) =>  {
+  var resultado = document.createElement('span');
+  resultado.innerHTML = templateStr;
+  output.appendChild(resultado);
 }
 
-palindrome("Rir, o breve verbo rir!");
-palindrome("Testando!!");
-palindrome("A grama é amarga.");
-palindrome("Racecar");
-palindrome("Eduardo");
+/* 05.20.19 - 40 min */
+function palindrome(str) {
+
+  var strCleaned = str.replace(/[\W_]/g, '').toLowerCase();
+  var strTested = strCleaned.split('').reverse().join("");
+  if ( strCleaned === strTested ) { return print(`É palindromo: ${strTested}`) }
+  
+  return print(`Não é palindromo: ${strTested}`);
+}
