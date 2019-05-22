4eaaad3cb4aebb229f7926a8d47defeffa8bb865
Eduardo Müller <edbmuller@gmail.com>
2019-05-21 22:01:50 -0300

    Palindrome 2° day

--- palindrome/index.js
+++ palindrome/index.js
@@ -1,14 +1,10 @@
-//  PALINDROME KATA!
-//  """""""""""""""""
-//  Get a string and check if its palindrome, beeing an word or frase that may contain non-alphanumerics characteres.
-
 var form = document.querySelector('.form');
 var input = document.querySelector('.input');
 var output = document.querySelector('.output');
 
 form.addEventListener('submit', function(evt) {
   evt.preventDefault();
-  palindrome(input.value);
+  palindrome2(input.value);
   input.value = '';
 });
 
@@ -17,6 +13,41 @@ const print = (templateStr) =>  {
   resultado.innerHTML = templateStr;
   output.appendChild(resultado);
 }
+
+
+//  PALINDROME KATA!
+//  """""""""""""""""
+//  Get a string and check if its palindrome, beeing an word or frase that may contain non-alphanumerics characteres.
+
+
+/* 05.21.19 - 8 min */
+
+const palindrome2 = (str) => {
+  let strCleaned = str.toLowerCase().replace(/[\W_]/g, '');
+  let strReversed = strCleaned.split('').reverse().join('');
+
+  if (strReversed === strCleaned) {
+    return print(`É palindromo: ${strReversed}`);
+  } 
+
+  return print(`Não palindromo: ${strReversed}`);
+}
+
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
 
 /* 05.20.19 - 40 min */
 function palindrome(str) {
